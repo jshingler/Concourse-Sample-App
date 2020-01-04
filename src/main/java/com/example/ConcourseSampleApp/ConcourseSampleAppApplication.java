074ab93191b938064ad8cd76495d28160def1f3c
Jim Shingler <ShinglerJim@gmail.com>
2020-01-04 16:35:55 -0500

    Better Sample App

---
+++ src/main/java/com/example/ConcourseSampleApp/ConcourseSampleAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.ConcourseSampleApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConcourseSampleAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConcourseSampleAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ConcourseSampleApp/ConcourseSampleAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ConcourseSampleApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConcourseSampleAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
